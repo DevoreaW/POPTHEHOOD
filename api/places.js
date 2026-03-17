a9d1759333ea4b74f8332ebe4a74d537c7a15f69
Devorea Wilson JR <devoreawilsonjr@Mac.lan>
2026-03-16 19:00:42 -0700

    increase rate limits

--- api/places.js
+++ api/places.js
@@ -4,7 +4,7 @@ import { sanitizeInput } from "./utils.js";
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(50, "1 h"),
+  limiter: Ratelimit.slidingWindow(100, "1 h"),
   analytics: true,
 });
 
